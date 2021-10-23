fbaf7ee356f01390d72f5e614d8badd0f04209c7
Kshitiz Pratap Singh <kshitiz100mit@gmail.com>
2021-10-23 23:55:53 +0530

    Home Page

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -1,8 +1,23 @@
 import React from "react";
 import classes from './Footer.module.css'
 
+import SynchroIcon from './FooterItems/SynchroIcon'
+import ErrorIcon from './FooterItems/ErrorIcon'
+import WarningIcon from './FooterItems/WarningIcon'
+import PowerIcon from './FooterItems/PowerIcon'
+import BellIcon from './FooterItems/BellIcon'
+
 const footer = () => (
     <div className = {classes.Footer}>
+        <div className={classes.MainGroup}>
+            <div className={classes.Main}><SynchroIcon /><span>main</span></div>
+            <div className={classes.Main}><ErrorIcon/>0 <WarningIcon style={{marginLeft:'0.3rem'}}/>0</div>
+        </div>
+        <div className={classes.SideGroup}>
+            <div className={classes.Main}><PowerIcon /><span style={{marginLeft:'0.3rem'}}>Powered by Next.js</span></div>
+            <BellIcon className={classes.Main}/>
+        </div>
+        
     </div>
 )
 
--- src/component/SideMenu/NarrowMenu/NarrowMenu.js
+++ src/component/SideMenu/NarrowMenu/NarrowMenu.js
@@ -5,23 +5,27 @@ import FilesIcon from "./NarrowMenuItems/FileIcon";
 import PencilIcon from "./NarrowMenuItems/PencilIcon";
 import TagIcon from './NarrowMenuItems/TagIcon';
 import MailIcon from './NarrowMenuItems/MailIcon'
+import SettingsIcon from "./NarrowMenuItems/SettingIcon";
 
 const narrowMenu = (props) => {
 
     return(
     <div className={classes.NarrowMenuContainer}>
-        <div 
-            onClick = {() => props.clicked(0)}
-            className = {[classes.Icon, props.showState[0] ? classes.active : null].join(' ')}><FilesIcon /></div>
-        <div 
-            onClick = {() => props.clicked(1)}
-            className = {[classes.Icon, props.showState[1] ? classes.active : null].join(' ')}><PencilIcon/></div>
-        <div 
-            onClick = {() => props.clicked(2)}
-            className = {[classes.Icon, props.showState[2] ? classes.active : null].join(' ')}><TagIcon /></div>
-        <div 
-            onClick = {() => props.clicked(3)}
-            className = {[classes.Icon, props.showState[3] ? classes.active : null].join(' ')}><MailIcon /></div>
+        <div className={classes.wrapper}>
+            <div 
+                onClick = {() => props.clicked(0)}
+                className = {[classes.Icon, props.showState[0] ? classes.active : null].join(' ')}><FilesIcon /></div>
+            <div 
+                onClick = {() => props.clicked(1)}
+                className = {[classes.Icon, props.showState[1] ? classes.active : null].join(' ')}><PencilIcon/></div>
+            <div 
+                onClick = {() => props.clicked(2)}
+                className = {[classes.Icon, props.showState[2] ? classes.active : null].join(' ')}><TagIcon /></div>
+            <div 
+                onClick = {() => props.clicked(3)}
+                className = {[classes.Icon, props.showState[3] ? classes.active : null].join(' ')}><MailIcon /></div>
+        </div>
+        <SettingsIcon className={classes.Setting}/>
     </div>
     ) 
 }
